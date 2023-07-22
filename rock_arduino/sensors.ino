0b937effe40d7c95edda06ab94a8be984c8675cb
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-07-21 20:35:41 -0700

    Add roll rate to sensor data

--- rock_arduino/sensors.ino
+++ rock_arduino/sensors.ino
@@ -14,17 +14,17 @@ void setupSensors() {
   }
 }
 
-// To fill out other values
-float buff;
-
-
 // Sensor values
 float accelx, accely, accelz;
+float gyrox, gyroy, gyroz;
 float baroAlt;
 unsigned long lastRead = millis();
 unsigned long dT;
 bool readSensors() {
   if (!IMU.readAcceleration(accelx, accely, accelz)) {
+    return false;
+  }
+  if (!IMU.readGyroscope(gyrox, gyroy, gyroz)) {
     return false;
   }
 
@@ -40,10 +40,12 @@ bool readSensors() {
 float accel; // Acceleration going up
 float alt;
 float vel;
+float roll;
 void predictPos() {
   vel = (baroAlt - alt) / (dT / 1000); // TODO: Use kalman filter to figure this out
   accel = accely; // TODO: Account for the angle of the rocket in this
   alt = baroAlt; // TODO: Use a 2d kalman filter to combine accel & baroAlt measurements
+  roll += gyrox / (dT / 1000); // TODO: use kalman filter to figure this out
 }
 
 float getAccel() {
