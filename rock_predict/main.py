15e612cc53496b36e926254e1932d20c79fd556f
Nv7-Github <junk4Nv7@gmail.com>
2024-08-05 22:18:53 -0700

    Simulate descent motor

--- rock_predict/main.py
+++ rock_predict/main.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
-from thrust import thrust
+from thrust import thrustAscent, thrustDescent
 import data
 
 # https://math.stackexchange.com/questions/721076/help-with-using-the-runge-kutta-4th-order-method-on-a-system-of-2-first-order-od
 
 # Constants
-p = 1.24
+p = 1.229
 A = 0.003425
-Cd = 0.57
-m = 0.569
+Cd = 0.38
+m = 0.949
 g = 9.81
-h = 13/1000
-P = 50
+h = 0.01
+P = 0
 I = 0
 D = 0
 
@@ -35,10 +35,12 @@ vi = 0
 def v(v):
     return v
 
+t_start = 100
 def a(t, v, real):
-    if (real):
-        return -(1/2)*p*A*(((Cd-0.57)*2.15)+0.49)*v*v/0.565 - g + thrust(t)/0.565
-    return -(1/2)*p*A*Cd*v*v/m - g + thrust(t)/m
+    global m
+    if v < 0:
+        m = 0.749
+    return -(1/2)*p*A*Cd*v*v/m - g + thrustAscent(t)/m + thrustDescent(t - t_start)/m
 
 # Calculates Cd from fin angle
 def cd(angle):
@@ -47,7 +49,7 @@ def cd(angle):
     if angle < 0:
         angle = 0
     global Cd
-    Cd = (0.0000142*angle*angle*angle + 0.001512*angle*angle - 0.0075534*angle)*0.1 + 0.57
+    Cd = (0.0000142*angle*angle*angle + 0.001512*angle*angle - 0.0075534*angle)*0.2 + 0.57
 
 # Solve using Runge-Kutta 4th order method
 def solve_iter(ti, xi, vi, real):
@@ -67,6 +69,13 @@ def solve_iter(ti, xi, vi, real):
         xi + (1/6) * (k0 + 2*k1 + 2*k2 + k3),
         vi + (1/6) * (l0 + 2*l1 + 2*l2 + l3)
     )
+    
+    """val = a(ti, vi, real)
+    v = vi + val * h
+    return (
+        xi + v*h + 0.5*val*h*h,
+        v
+    )"""
 
 def get_apogee(ti, vi, xi):
     t = ti
@@ -92,12 +101,18 @@ control = 0
 err_i = 0
 err_d = 0
 v_val = 0
-while vi > 0 or t == 0:
+ind = 0
+while xi > 0 or t < 0.1:
     res = solve_iter(t, xi, vi, True)
     xi = res[0]
     vi = res[1]
     v_val += a(t, v_val, True)*0.9*h
     t += h
+    while data.time[ind] < t:
+        ind += 1
+        if ind >= len(data.time)-1:
+            ind = len(data.time)-1
+            break
 
     t_plt.append(t)
     x_plt.append(xi)
@@ -121,13 +136,18 @@ while vi > 0 or t == 0:
         cd(control)
         #print(err, control)
         ang_plt.append(control)
+
+        # Control descent motor
+        if (vi < 0):
+            m = 0.649
+        if (vi < -80 and t_start > 99):
+            t_start = t + 0.5
     else:
        pre_plt.append(get_apogee(t, v_val, xi) - 259.08*1.016)
-       ang_plt.append(5)
-       cd(5)
-    if (t >= 1):
-       print(t, xi)
-   #print(t, xi, vi)
+       ang_plt.append(0)
+       cd(0)
+    
+    #print(t, xi, vi, a(t, vi, True), thrustAscent(t))
 
 fig, ax = plt.subplots(2, 2,figsize=(15, 8))
 fig.subplots_adjust(
@@ -138,26 +158,21 @@ fig.subplots_adjust(
 )
 ax2 = ax[0, 0].twinx()
 ax2.plot(t_plt, x_plt)
-ax2.plot(data.time, data.alt, "-.")
 ax2.set_xlabel("Time (s)")
 ax2.set_ylabel("Altitude (m)")
 ax[0, 0].plot(t_plt, pre_plt, "-.", color="red")
-ax[0, 0].plot(data.time, data.error, ":", color="red")
 ax[0, 0].set_ylabel("Predicted Apogee Error (m)")
 
 ax[0, 1].plot(t_plt, v_plt)
-ax[0, 1].plot(data.time, data.vel, "-.", color="orange")
 ax[0, 1].set_xlabel("Time (s)")
 ax[0, 1].set_ylabel("Velocity (m/s)")
 
 ax[1, 0].plot(t_plt, a_plt)
-ax[1, 0].plot(data.time, data.accel, ":", color="orange")
 ax[1, 0].plot([0, t_plt[-1]], [0, 0], "-.")
 ax[1, 0].set_xlabel("Time (s)")
 ax[1, 0].set_ylabel("Acceleration (m/s^2)")
 
 ax[1, 1].plot(t_plt, ang_plt)
-ax[1, 1].plot(data.time, data.angle, "-.", color="orange")
 ax[1, 1].set_xlabel("Time (s)")
 ax[1, 1].set_ylabel("Canard Angle (deg)")
 
--- rock_predict/thrust.py
+++ rock_predict/thrust.py
@@ -1,22 +1,44 @@
 import csv
 
-FILE = "AeroTech_F67W.csv"
+ASCENT = "AeroTech_G80T.csv"
 
-thrustcurve = []
-with open(FILE, "r") as f:
+thrustcurveAscent = []
+thrustcurveDescent = []
+with open(ASCENT, "r") as f:
     r = csv.reader(f)
     for row in r:
         try:
-            thrustcurve.append((float(row[0]), float(row[1])))
+            thrustcurveAscent.append((float(row[0]), float(row[1])))
         except ValueError: # Headers
             continue
 
-def thrust(ti):
-    for i in range(0, len(thrustcurve)):
-        if ti < thrustcurve[i][0]:
+DESCENT = "AeroTech_F35W.csv"
+with open(DESCENT, "r") as f:
+    r = csv.reader(f)
+    for row in r:
+        try:
+            thrustcurveDescent.append((float(row[0]), float(row[1])))
+        except ValueError: # Headers
+            continue
+
+def thrustAscent(ti):
+    for i in range(0, len(thrustcurveAscent)):
+        if ti < thrustcurveAscent[i][0]:
             if i == 0:
-                return thrustcurve[i][1]
+                return thrustcurveAscent[i][1]
         
-            return ((ti - thrustcurve[i-1][0])/(thrustcurve[i][0] - thrustcurve[i-1][0])) * (thrustcurve[i][1] - thrustcurve[i-1][1]) + thrustcurve[i-1][1]
+            return ((ti - thrustcurveAscent[i-1][0])/(thrustcurveAscent[i][0] - thrustcurveAscent[i-1][0])) * (thrustcurveAscent[i][1] - thrustcurveAscent[i-1][1]) + thrustcurveAscent[i-1][1]
+        
+    return 0
+
+def thrustDescent(ti):
+    if ti < 0:
+        return 0
+    for i in range(0, len(thrustcurveDescent)):
+        if ti < thrustcurveDescent[i][0]:
+            if i == 0:
+                return thrustcurveDescent[i][1]
+        
+            return ((ti - thrustcurveDescent[i-1][0])/(thrustcurveDescent[i][0] - thrustcurveDescent[i-1][0])) * (thrustcurveDescent[i][1] - thrustcurveDescent[i-1][1]) + thrustcurveDescent[i-1][1]
         
     return 0
